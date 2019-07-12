a0b982da281cbc5a6ecb3decb198813301b80bc2
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2019-07-12 14:10:22 +0300

    add models

--- chat_server/api/models.py
+++ chat_server/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Chat(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    users = models.ManyToManyField(User)
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
