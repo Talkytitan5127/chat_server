099c14ae3cb4dbd689a74adfd0cb69a6d088d36e
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2019-07-12 23:44:47 +0300

    remove admin.py;  add get_list_message and get_list_chat

--- chat_server/api/models.py
+++ chat_server/api/models.py
@@ -25,4 +25,4 @@ class Message(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"From {str(self.author)}"
+        return f"From {str(self.author)}:\n{self.text}"
--- chat_server/api/views.py
+++ chat_server/api/views.py
@@ -10,6 +10,7 @@ def read_data(request):
     return data
 
 def user_add(request):
+    print(request)
     if request.method != 'POST':
         return HttpResponseNotAllowed(permitted_methods=['POST'])
     
@@ -56,7 +57,7 @@ def send_message(request):
     data = read_data(request)
 
     chat = get_object_or_404(Chat, id=data['chat'])
-    user = get_object_or_404(User, id=data['author'])
+    user = get_object_or_404(chat.users, id=data['author'])
     
     message = Message.objects.create(chat=chat, author=user, text=data['text'])
     
@@ -65,8 +66,44 @@ def send_message(request):
         })
 
 def get_list_chats(request):
-    pass
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(permitted_methods=['POST'])
+
+    data = read_data(request)
+
+    user = get_object_or_404(User, id=data['user'])
+    chats = user.chat_set.all()
+
+    list_chats = []
+    for chat in sorted(chats,
+        key=lambda chat:chat.message_set.order_by('created_at').last().created_at, reverse=True):
+        packet = {
+            'chat_id': chat.id,
+            'name': chat.name,
+            'created_at': chat.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'users_id': list(map(lambda user: user.id, chat.users.all())),
+        }
+        list_chats.append(packet)
+
+    return JsonResponse({'chats':list_chats})
 
 
 def get_list_messages(request):
-    pass
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(permitted_methods=['POST'])
+
+    data = read_data(request)
+
+    messages = Message.objects.filter(chat=data['chat']).order_by('created_at')
+    
+    list_messages = []
+    for message in messages:
+        packet = {
+            'chat_id': message.chat.id,
+            'author_id': message.author.id,
+            'text': message.text,
+            'created_at': message.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        }
+        list_messages.append(packet)
+    
+    return JsonResponse({'messages':list_messages})
