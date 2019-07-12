68f10364995edb5a116f7833e5a003a41e4c6f16
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2019-07-12 14:24:16 +0300

    add str presentation

--- chat_server/api/models.py
+++ chat_server/api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=128, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
 
 
 class Chat(models.Model):
@@ -11,9 +14,15 @@ class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     users = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.name
+
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {str(self.author)}"
